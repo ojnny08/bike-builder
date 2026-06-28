b025f504bd904c90d72377294562b687613af652
Jonathan Li <Jonathan.lx01@gmail.com>
2026-06-28 16:13:23 -0400

    changed styling

--- bikebuilder/apps/builds/views.py
+++ bikebuilder/apps/builds/views.py
@@ -58,6 +58,7 @@ class MyBuildViewSet(ViewSet):
         except Build.DoesNotExist:
             return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
         build.delete()
+        delete_from_s3(build.image_url)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def partial_update(self, request, pk=None):
